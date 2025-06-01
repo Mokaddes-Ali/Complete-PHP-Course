260be8a43f5633b4340fc78e00f66b9baf0c7dd1
Mokaddes <mokaddes.ru2000@gmail.com>
2025-06-01 06:01:57 +0600

    first commit

---
+++ php-class01/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body style="background-color: aliceblue; font-size: 40px; margin-left: 100px;">
+    <?php
+    
+    $personName = "Mokaddes Ali";
+    
+    echo $personName;
+
+    // Best Practise
+    
+    // $person_name............... snake_case($personName/variable/function);
+    
+
+
+   ?>
+
+</body>
+
+</html>
