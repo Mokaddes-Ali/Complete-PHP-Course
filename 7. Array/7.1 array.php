01d5b08608129b14f4ae75b5a34ee483d12cfd98
Mokaddes <mokaddes.ru2000@gmail.com>
2025-06-16 17:46:24 +0600

    complete class 7 array bulit in function

--- 7. Array/7.1 array.php
+++ 7. Array/7.1 array.php
@@ -23,6 +23,10 @@ print_r($number);
 var_dump($number);
 */
 
+
+//associative array
+
+/*
 $students = [
     // 'key' => 'value'
     'name' => 'M. Ali',
@@ -44,6 +48,9 @@ print_r($fruits);
 
 $fruitsStr = implode("-",$fruits);
 print_r($fruitsStr);
+
+
+//Multi Dimensional Array
 
 $studentInfo = [
     ['name' => 'Ab. Khalek', 'age' => 20],
@@ -71,3 +78,108 @@ print_r($serialize);
 // string to json data convert
 $jsonData = json_encode($data);
 print_r($jsonData);
+*/
+
+
+// ForEach => In array all elements check by foreach
+
+/*
+//index array 
+$fruits = ["Mango", "Banana", "Rice"];
+// as means accept
+foreach($fruits as $fruit){
+    echo $fruit . " ";
+    
+};
+
+echo "\n";
+echo "\n";
+*/
+
+
+/*
+//associative array
+$personInforms = [
+    'name' => 'Mokaddes Ali',
+    'age' => 24,
+    'address' => 'Rajshahi',
+    'phone' => '01402967304',
+];
+foreach ( $personInforms as $key => $value ){
+    echo "$key : $value\n";
+};
+ print_r($personInforms);
+ */
+
+ 
+/*
+ $personInforms = [
+    'name' => 'Mokaddes Ali',
+    'age' => 24,
+    'address' => 'Rajshahi',
+    'phone' => '01402967304',
+];
+
+// use unset function delete first element of array
+
+// unset($personInforms['name']);
+print_r($personInforms);
+
+*/
+
+/*
+//Array Filter Data
+
+$data = ['Mango', '', null];
+
+$filtered = array_filter($data);
+print_r($filtered ); 
+
+*/
+
+
+$user =[
+    'name' => 'Mokaddes Ali',
+    'age' => 24,
+    'address' => 'Rajshahi',
+    'phone' => '01402967304',
+    'email' => 'mokaddes.ru2000gmail.com',
+];
+
+//use extract built in function than array all key working as a variable
+extract($user);
+ echo $name; 
+ 
+ // a array divided in defferent part by array_slice built in function 
+ $numbers = range(1,15);
+ $sliced = array_slice($numbers, 5,5);
+ print_r($numbers);
+ print_r($sliced);
+ 
+
+ // merge => merge multiple array in one array
+ $arr1 = ['mango', 'jackfruit'];
+ $arr2 = ['Banana','Pineapple'];
+
+ $merged = array_merge( $arr1, $arr2);
+ print_r($merged );
+
+//  search by in_array built in function
+ $fruits = ['mango', 'jackfruit','Banana'];
+ 
+ if(in_array('mango',$fruits)){
+    echo "Found this fruit";
+ }else{
+    echo"Not Found this serach!";
+ }
+echo "\n";
+
+ $arr1 = ['mango', 'jackfruit'];
+ $arr2 = ['mango','Pineapple'];
+
+ $difference = array_diff( $arr1,  $arr2);
+ 
+//  print_r($difference);
+// number 1 theke 15 prjnto jabe but difference 3 hbe
+$numbers = range(1,15, 3); 
+print_r($numbers);
