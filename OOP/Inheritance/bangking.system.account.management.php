e9a4346a18c864e6bb0e796421d6adece2802da4
Mokaddes Ali <mokaddes.ru200@gmail.com>
2025-07-06 18:23:33 +0600

    complete inheritance cimmit

--- OOP/Inheritance/bangking.system.account.management.php
+++ OOP/Inheritance/bangking.system.account.management.php
@@ -1,9 +1,55 @@
-<!-- bangking.system.account.management. -->
+
 
 <?php
-
+ //<-- bangking.system.account.management. -->
 class Account{
     public $accountNumber;
+    public $balance;
+    public function __construct($accountNumber, $balance){
+        $this->accountNumber = $accountNumber;
+        $this->balance = $balance;
+    }
+    public function getBalance(){
+        return "Account Number:{$this->accountNumber}, Balance:{$this->balance}";
+    }
     
 }
+
+class SavingsAccount extends Account{
+
+    public $interestRate;
+    public function __construct($accountNumber, $balance ,$interestRate){
+        parent::__construct($accountNumber, $balance);
+        $this->interestRate = $interestRate;
+
+    }
+
+    public function calculateInterest(){
+        return $this->balance * $this->interestRate / 100;
+    }
+
+    public function getBalance(){
+        $interest = $this->calculateInterest();
+
+        return parent::getBalance() . " ," . "Interest Eraned: $interest";
+    }
+}
+
+class CurrentAccount extends Account{
+    public $overdraftLimit;
+    public function __construct($accountNumber, $balance, $overdraftLimit){
+        parent::__construct($accountNumber, $balance);
+        $this->overdraftLimit = $overdraftLimit;
+    }
+
+    public function getBalance(){
+        return parent::getBalance() . " ," . "OverDraftLimit: {$this->overdraftLimit}";
+    }
+}
+
+$savings = new SavingsAccount("SA123",340,20);
+echo $savings->getBalance() . "\n";
+
+$current = new CurrentAccount("Ali123",200,10);
+echo $current->getBalance();
 ?>
